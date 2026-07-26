6eb895ff3ce77406b1d7ef2b33d1278cc1a74f96
lum8rjack <4932619+lum8rjack@users.noreply.github.com>
2026-07-26 15:04:27 -0500

    updated URL and parsing again

--- stl_superpark/sensor.py
+++ stl_superpark/sensor.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 from datetime import timedelta
 import requests
-from lxml import html
 import voluptuous as vol
 import logging
 
@@ -15,17 +14,8 @@ from homeassistant.helpers.entity import Entity
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "STL Super Park"
-DEFAULT_URL = "https://www.flystl.com/parking-transportation/"
+DEFAULT_URL = "https://countsstl.abacusai.app/api/counts"
 SCAN_INTERVAL = timedelta(minutes=15)
-LOCATION_DICTIONARY = {
-    "Terminal 1": '//*[@id="parking"]/div/div[2]/div/div[1]/section[1]/a/div[1]/div[2]/p[1]',
-    "Terminal 2": '//*[@id="parking"]/div/div[2]/div/div[1]/section[2]/a/div[1]/div[2]/p[1]',
-    "Lot A": '//*[@id="parking"]/div/div[2]/div/div[1]/section[3]/a/div[1]/div[2]/p[1]',
-    "Lot B": '//*[@id="parking"]/div/div[2]/div/div[1]/section[4]/a/div[1]/div[2]/p[1]',
-    "Lot C": '//*[@id="parking"]/div/div[2]/div/div[1]/section[5]/a/div[1]/div[2]/p[1]',
-    "Lot D": '//*[@id="parking"]/div/div[2]/div/div[1]/section[6]/a/div[1]/div[2]/p[1]',
-    "Lot E": '//*[@id="parking"]/div/div[2]/div/div[1]/section[7]/a/div[1]/div[2]/p[1]'
-}
 
 # Validation of the user's configuration
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
@@ -115,28 +105,62 @@ class STLSuperParkData:
         # Set headers
         custom_headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36","Accept-Lanugage": "en-US,en;q=0.9"}
 
-	# Send request
-        page = requests.get(DEFAULT_URL, headers=custom_headers, timeout=5.0)
-        tree = html.fromstring(page.content)
+        # Send request
+        response = requests.get(DEFAULT_URL, headers=custom_headers, timeout=5.0)
+        response.raise_for_status()
 
-        # Get element using XPath
-        for key, value in LOCATION_DICTIONARY.items():
-            availability = tree.xpath(value)
-            percent = availability[0].text_content().strip().replace("%", "")
-            if percent.upper() == "FULL":
-                percent = 0
+        # Parse JSON directly to a Python dictionary
+        data = response.json()
 
-            if "Terminal 1" == key:
-                self.spaces["t1"] = percent
-            elif "Terminal 2" == key:
-                self.spaces["t2"] = percent
-            elif "Lot A" == key:
-                self.spaces["a"] = percent
-            elif "Lot B" == key:
-                self.spaces["b"] = percent
-            elif "Lot C" == key:
-                self.spaces["c"] = percent
-            elif "Lot D" == key:
-                self.spaces["d"] = percent
-            elif "Lot E" == key:
-                self.spaces["e"] = percent
+        # Get data for each lot
+        lots = {lot["lot"]: lot for lot in data}
+
+        # Example response
+        """
+        [
+            {
+                "lot": "Lot C",
+                "capacity": 3019,
+                "avail": 1000
+            },
+            {
+                "lot": "Lot E",
+                "capacity": 233,
+                "avail": 52
+            },
+            {
+                "lot": "Term 2",
+                "capacity": 975,
+                "avail": -7
+            },
+            {
+                "lot": "Term 1",
+                "capacity": 1850,
+                "avail": 663
+            },
+            {
+                "lot": "Lot B",
+                "capacity": 481,
+                "avail": 63
+            },
+            {
+                "lot": "Lot A",
+                "capacity": 973,
+                "avail": 294
+            },
+            {
+                "lot": "Lot D",
+                "capacity": 1310,
+                "avail": 68
+            }
+        ]
+        """
+
+        # Calculate % available for each lot
+        self.spaces["t1"] = str(int((lots["Term 1"]["avail"] / lots["Term 1"]["capacity"]) * 100))
+        self.spaces["t2"] = str(int((lots["Term 2"]["avail"] / lots["Term 2"]["capacity"]) * 100))
+        self.spaces["a"] = str(int((lots["Lot A"]["avail"] / lots["Lot A"]["capacity"]) * 100))
+        self.spaces["b"] = str(int((lots["Lot B"]["avail"] / lots["Lot B"]["capacity"]) * 100))
+        self.spaces["c"] = str(int((lots["Lot C"]["avail"] / lots["Lot C"]["capacity"]) * 100))
+        self.spaces["d"] = str(int((lots["Lot D"]["avail"] / lots["Lot D"]["capacity"]) * 100))
+        self.spaces["e"] = str(int((lots["Lot E"]["avail"] / lots["Lot E"]["capacity"]) * 100))
